54f1c1bee45a88a03a1f1ca8b67ca2e8b5aa1c73
“nidumolu” <snidumolu@gmail.com>
2021-10-17 15:15:41 +0530

    IceCream webservice

---
+++ go.mod
@@ -0,0 +1,6 @@
+module sample/icecream-webservice
+
+go 1.15
+
+require github.com/gin-gonic/gin v1.7.4
+
---
+++ main.go
@@ -0,0 +1,66 @@
+package main 
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type icecream struct
+{
+
+	ID string `json:"id"`
+	Flavor string `json:"flavor"`
+	Price float64 `json:"price"`
+}
+
+var icecreams = []icecream{
+	{ID: "1", Flavor: "Vanilla", Price: 1.99},
+	{ID: "2", Flavor: "Chocolate", Price: 2.99},
+	{ID: "3", Flavor: "Strawberry", Price: 2.99},
+  
+}
+
+
+func getIcecreams (c *gin.Context)
+{
+	c.IndentedJSON(http.StatusOK, icecreams)
+}
+
+func postIcecreams(c *gin.Context){
+	var newIcecream icecream
+	// Call BindJSON to bind the received JSON to
+    // newIcecream.
+    if err := c.BindJSON(&newIcecream); err != nil {
+        return
+    }
+
+	// Add the new icecream to the slice.
+    icecreams = append(icecreams, newIcecream)
+    c.IndentedJSON(http.StatusCreated, newIcecream)
+}
+
+// getIcecreamByID locates the icecream whose ID value matches the id
+// parameter sent by the client, then returns that icecream as a response.
+func getIcecreamByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of icecreams, looking for
+    // an icecream whose ID value matches the parameter.
+    for _, a := range icecreams {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "That icecream was not found :-( "})
+}
+
+
+func main()
+{
+	router := gin.Default()
+	router.GET("/icecreams", getIcecreams)
+	router.POST("/icecreams", postIcecreams)
+	router.GET("/icecreams/:id", getIcecreamByID)
+	router.Run("localhost:8080")
+}
